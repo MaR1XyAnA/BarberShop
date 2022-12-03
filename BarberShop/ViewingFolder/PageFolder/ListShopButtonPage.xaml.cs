9a504fdfb6f8dfa0f0ea9d1dc46cf85eef9d32f2
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2022-12-03 18:24:18 +0300

    dfsfdорлрол

--- BarberShop/ViewingFolder/PageFolder/ListShopButtonPage.xaml.cs
+++ BarberShop/ViewingFolder/PageFolder/ListShopButtonPage.xaml.cs
@@ -23,6 +23,7 @@ namespace BarberShop.ViewingFolder.PageFolder
         {
             InitializeComponent();
             FrameClass.AddEditServiseClass = InformationServisFrame;
+            FrameClass.AddEditServiseClass.Navigate(new NextThreeTextPage());
             AppConnectClass.DataBase = new BarberShopDataBaseEntities(); // Подключаем Базу Данных к этой странице.
             ListShopButtonListBox.ItemsSource = AppConnectClass.DataBase.HaircutTable.ToList(); // В ListWorkwrListBox, в ItemsSource выводим всю информацию из таблицы WorkerTable в виде списка.
         }
@@ -30,6 +31,32 @@ namespace BarberShop.ViewingFolder.PageFolder
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void ListShopButtonListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            HaircutTable haircutTable = (HaircutTable)ListShopButtonListBox.SelectedItem;
+            FrameClass.AddEditServiseClass.Navigate(new InformationServisePage(haircutTable));
+            ButtonStackPanel.Visibility = Visibility.Visible;
+        }
+
+        private void EditInformationsServisButton_Click(object sender, RoutedEventArgs e)
+        {
+            //HaircutTable haircutTable = (HaircutTable)ListShopButtonListBox.SelectedItem;
+            //FrameClass.AddEditServiseClass.Navigate(new InformationServisePage(haircutTable));
+        }
+
+        private void DeliteServiseButton_Click(object sender, RoutedEventArgs e) // Событие кнопки на удаление услуги из БД
+        {
+            if (MessageBox.Show("Вы действительно хотите удалить данную услугу?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes) // Выводим сообщение, и если пользователь нажал кнопку "Да" то;
+            {
+                var DaliteHaircut = ListShopButtonListBox.SelectedItem as HaircutTable; // В переменную "DaliteHaircut" получаем данные из таблицы "HaircutTable" по услуге выбронному в элементе "ListShopButtonListBox";
+                AppConnectClass.DataBase.HaircutTable.Remove(DaliteHaircut); // Удаляем услугу из таблицы "HaircutTable";
+                AppConnectClass.DataBase.SaveChanges(); // Сохраняем БД;
+                ListShopButtonListBox.ItemsSource = AppConnectClass.DataBase.HaircutTable.ToList(); // Выводим в элемент "ListShopButtonListBox" данные из таблицы "HaircutTable";
+                FrameClass.AddEditServiseClass.Navigate(new NextThreeTextPage());// Открываем страницу "NextThreeTextPage";
+                ButtonStackPanel.Visibility = Visibility.Collapsed; // ВЫключаем видемость элементу "ButtonStackPanel", в котором находятся кнопки управления;
+            }
         }
     }
 }
